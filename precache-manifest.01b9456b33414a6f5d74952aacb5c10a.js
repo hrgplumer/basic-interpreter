73a0f358c4885d793280000556db8accc6617517
John Connor <hrgplumer@gmail.com>
2019-12-13 12:39:17 -0500

    Updates

---
+++ precache-manifest.01b9456b33414a6f5d74952aacb5c10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35334683fc6c08ce7b6fe7b2aa7b8046",
+    "url": "/basic-interpreter/index.html"
+  },
+  {
+    "revision": "9d303ddbdfb2e8198334",
+    "url": "/basic-interpreter/static/css/main.f58f9491.chunk.css"
+  },
+  {
+    "revision": "24413d7a40e7026f4ab5",
+    "url": "/basic-interpreter/static/js/2.9b0bb7ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/basic-interpreter/static/js/2.9b0bb7ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d303ddbdfb2e8198334",
+    "url": "/basic-interpreter/static/js/main.303d490a.chunk.js"
+  },
+  {
+    "revision": "9f215b7784348dc8b834",
+    "url": "/basic-interpreter/static/js/runtime-main.256542fb.js"
+  }
+]);
