77bcd2a8fb0df61d749b570d029714d562fb2174
John Connor <hrgplumer@gmail.com>
2019-12-19 15:56:32 -0500

    add jest tests

--- basic-interpreter/src/App.js
+++ basic-interpreter/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import useTerminalEvents from './hooks/useTerminalEvents';
 import './App.css';
 import './crt.css';
+import { fireEvent } from '@testing-library/react';
 
 const App = () => {
   const [terminalText, setTerminalText] = useState('');
@@ -20,9 +21,10 @@ const App = () => {
           id="terminal"
           name="terminal"
           value={terminalText}
+          aria-label="terminal"
           autoFocus
           onChange={(e) => handleTerminalChange(e)}
-          onKeyPress={(e) => handleTerminalKeyPress(e)}
+          onKeyPress={(e) => handleTerminalKeyPress(e)} // Add another method here to capture input! Delete whatever it captures?
         />
       </div>
       <div className="overlay">AV-1</div>
--- basic-interpreter/src/App.test.js
+++ basic-interpreter/src/App.test.js
@@ -1,9 +1,78 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = () => {
+  const utils = render(<App />);
+  return {
+    ...utils
+  };
+}
+
+afterEach(cleanup);
+
+test('renders commodore 64 header', () => {
+  const { getByText } = setup();
+  const textElement = getByText(/commodore 64/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('overlay shows up', () => {
+  const { getByText } = setup();
+  const overlay = getByText('AV-1');
+  expect(overlay).toBeInTheDocument();
+});
+
+test('terminal shows up', () => {
+  const { getByLabelText } = setup();
+  const terminal = getByLabelText('terminal');
+  expect(terminal).toBeInTheDocument();
+});
+
+test('small print program runs', () => {
+  const { getByLabelText } = setup();
+  const terminal = getByLabelText('terminal');
+  expect(terminal).toBeInTheDocument();
+
+  fireEvent.change(terminal, { target: { value: "10 print \"hello world\"\nrun" } });
+  fireEvent.keyPress(terminal, { charCode: 13 }); // press enter
+  expect(terminal.value).toBe(`10 print "hello world"
+run
+hello world
+READY.`);
+});
+
+test('for loops', () => {
+  const { getByLabelText } = setup();
+  const terminal = getByLabelText('terminal');
+  expect(terminal).toBeInTheDocument();
+
+  fireEvent.change(terminal, { target: { value: "10 for i = 0 to 10 step 1\n20 print i\n30 next\nrun" } });
+  fireEvent.keyPress(terminal, { charCode: 13 });
+  expect(terminal.value).toBe(`10 for i = 0 to 10 step 1
+20 print i
+30 next
+run
+0
+
+1
+
+2
+
+3
+
+4
+
+5
+
+6
+
+7
+
+8
+
+9
+
+10
+READY.`);
 });
